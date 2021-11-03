91950acca0ade3b8b9aeb074f84ef0fa746e5653
Stefano Martino <stefano97martino@gmail.com>
2021-11-03 15:41:02 +0100

    Second Panel

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,9 @@
+//ALways start at the first panel
+const panels = ["first-panel", "second-panel", "third-panel"];
+document.getElementById(panels[0]).scrollIntoView();
+let currentPanel = 0;
+let lastScrollTop = 0;
+
 //Add Scroll functions
 document.getElementById("first-panel-button").onclick = function () {
   $("html, body").animate(
@@ -16,3 +22,25 @@ document.getElementById("second-panel-button").onclick = function () {
     1000
   );
 };
+
+window.ontouchmove = function () {
+  console.log("Hello");
+};
+
+/*const _window = $(window);
+_window.scroll(function (event) {
+  const currentScrollTop = $(this).scrollTop();
+  if (currentScrollTop > lastScrollTop) {
+    //downscroll
+    if (currentPanel > 0) {
+      currentPanel--;
+    }
+  } else {
+    //upscroll
+    if (currentPanel < panels.length - 1) {
+      currentPanel++;
+    }
+  }
+
+  lastScrollTop = currentScrollTop;
+});*/
